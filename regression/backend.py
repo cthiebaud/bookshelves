30b49021366e61738d73e62c3186b8920912e164
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-11-28 16:51:52 +0100

    three regressions

--- regression/backend.py
+++ regression/backend.py
@@ -1,4 +1,3 @@
-from sklearn.preprocessing import StandardScaler
 from flask import Flask, jsonify, render_template, request
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -11,24 +10,13 @@ CORS(app)
 def index():
     return render_template('index.html')
 
-@app.route('/perform_regression', methods=['POST'])
-def perform_regression():
-    data = request.get_json()
-
-    # Extract data
-    X1 = data['X1']
-    X2 = data['X2']
-    Y = data['Y']
-
+def perform_regression_and_get_std_dev_and_model(X1, X2, Y):
     # Reshape the data for sklearn
     X = np.column_stack((X1, X2))
 
     # Perform linear regression
     model = LinearRegression()
     model.fit(X, Y)
-
-    # Get coefficients
-    coefficients = {'intercept': model.intercept_, 'coef_X1': model.coef_[0], 'coef_X2': model.coef_[1]}
 
     # Predict Y values
     Y_pred = model.predict(X)
@@ -39,21 +27,65 @@ def perform_regression():
     # Calculate standard deviation of residuals
     std_dev_residuals = np.std(residuals)
 
+    print(f"std_dev_residuals: {std_dev_residuals}")
+
+    return std_dev_residuals, model
+
+@app.route('/perform_regression', methods=['POST'])
+def perform_regression():
+    data = request.get_json()
+
+    # Extract data
+    xyz = [
+        data['x'],
+        data['y'],
+        data['z']
+    ]
+
+    p = [
+        [0,1,2],
+        [2,1,0],
+        [0,2,1],    
+    ]
+
+    res = []
+
+    for index, _p in enumerate(p):
+        res.append(perform_regression_and_get_std_dev_and_model(xyz[_p[0]], xyz[_p[1]], xyz[_p[2]]))
+    # Calculate std_dev_residuals and get the model for the original data
+    ## std_dev_original, model_original = perform_regression_and_get_std_dev_and_model(xyz[p[0][0]], xyz[p[0][1]], xyz[p[0][2]])
+    ## std_dev_permuta1, model_permuta1 = perform_regression_and_get_std_dev_and_model(xyz[p[1][0]], xyz[p[1][1]], xyz[p[1][2]])
+    ## std_dev_permuta2, model_permuta2 = perform_regression_and_get_std_dev_and_model(xyz[p[2][0]], xyz[p[2][1]], xyz[p[2][2]])
+
+    print(res)
+
+    # Find the permutation with the lowest std_dev_residuals
+    P, best_permutation = min(
+        enumerate(res),
+        key=lambda x: x[1][0]
+    )
+    std_dev_residuals = best_permutation[0]
+    model = best_permutation[1]
+    
+    print(p[P], "model.coef_", model.coef_, "model.intercept_", model.intercept_)
+
     # Create a meshgrid for the plane
     n_points = 16
     X1_plane = np.linspace(0, 256, n_points)
     X2_plane = np.linspace(0, 256, n_points)
     X1_plane, X2_plane = np.meshgrid(X1_plane, X2_plane)
-    Y_plane = coefficients['intercept'] + coefficients['coef_X1'] * X1_plane + coefficients['coef_X2'] * X2_plane
+    Y_plane = model.intercept_ + model.coef_[0] * X1_plane + model.coef_[1] * X2_plane
 
-    # Coerce z-coordinates onto the regression plane
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    planes = [X1_plane.tolist(), X2_plane.tolist(), Y_plane.tolist()]
+    coeffs = [model.coef_[0], model.coef_[1], -1]
+
+    coefficients = {'intercept': model.intercept_, 'x': coeffs[p[P][0]], 'y': coeffs[p[P][1]], 'z': coeffs[p[P][2]]}
+    meshgrid =  {'x': planes[p[P][0]], 'y': planes[p[P][1]], 'z': planes[p[P][2]]}
 
     # Prepare results to send back to the frontend
     results = {
         'coefficients': coefficients,
-        'meshgrid': {'X1': X1_plane.tolist(), 'X2': X2_plane.tolist(), 'Y': Y_plane.tolist()},
+        'meshgrid': meshgrid,
         'std_dev_residuals': std_dev_residuals
     }
 
@@ -97,4 +129,4 @@ def perform_regression2d():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=True, host='localhost', port=4321)
